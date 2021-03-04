204600acb91c457f5512917bc315e707560f2752
Jacob Floyd <jacob.floyd@copart.com>
2021-03-04 17:30:36 -0600

    Update kafka-python to latest 2.0

--- actions/produce.py
+++ actions/produce.py
@@ -1,6 +1,21 @@
+import six
+
 from st2common.runners.base_action import Action
-from kafka import SimpleProducer, KafkaClient
-from kafka.util import kafka_bytestring
+from kafka import KafkaProducer
+from kafka.producer.future import RecordMetadata
+
+
+# copied from kafka-python <1.0
+def kafka_bytestring(s):
+    """
+    Takes a string or bytes instance
+    Returns bytes, encoding strings in utf-8 as necessary
+    """
+    if isinstance(s, six.binary_type):
+        return s
+    if isinstance(s, six.string_types):
+        return s.encode('utf-8')
+    raise TypeError(s)
 
 
 class ProduceMessageAction(Action):
@@ -36,10 +51,13 @@ class ProduceMessageAction(Action):
         # set default for empty value
         _client_id = self.config.get('client_id') or self.DEFAULT_CLIENT_ID
 
-        client = KafkaClient(_hosts, client_id=_client_id)
-        client.ensure_topic_exists(topic)
-        producer = SimpleProducer(client)
-        result = producer.send_messages(topic, kafka_bytestring(message))
-
-        if result[0]:
-            return result[0]._asdict()
+        producer = KafkaProducer(
+            bootstrap_servers=_hosts.split(','),
+            client_id=_client_id,
+            # TODO: Support security_protocol + sasl_mechanism + sasl_*
+        )
+        future = producer.send(topic=topic, value=kafka_bytestring(message))
+        producer.flush()
+        if not future.failed():
+            result = future.get()  # type: RecordMetadata
+            return result._asdict()
