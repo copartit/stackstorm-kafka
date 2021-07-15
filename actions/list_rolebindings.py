b43d43277d8141b5bde74053338b6c412e9cbd24
Thangaraj Subramani <thangaraj.subramani@copart.com>
2021-07-15 17:43:37 -0500

    fix role binding

---
+++ actions/list_rolebindings.py
@@ -0,0 +1,45 @@
+from rbac.mds import MetaDataService
+
+from typing import Dict, Optional
+
+from lib.base_admin_action import BaseAdminAction
+
+
+class ListRoleBindingAction(BaseAdminAction):
+    def run(
+        self,
+        kafka_broker,
+        mds_port,
+        principal,
+        role,
+        mds_username,
+        mds_password,
+    ):
+        mds = MetaDataService(mds_username, mds_password, kafka_broker, port=mds_port)
+        return mds.list_rolebinding_resources(principal, role)
+
+
+if __name__ == "__main__":
+    import os
+
+    KAFKA_BROKER = os.environ.get("KAFKA_BROKER",'c-kafka-qa4-rn.copart.com')
+    MDS_PORT = os.environ.get("MDS_PORT", 8090)
+    MDS_USERNAME = os.environ.get("MDS_USERNAME")
+    MDS_PASSWORD = os.environ.get("MDS_PASSWORD")
+    PRINCIPAL = os.environ.get("PRINCIPAL", "User:test-qa-consumer")
+    ROLE = os.environ.get("ROLE", "DeveloperRead")
+
+    action = ListRoleBindingAction()
+
+    res = action.run(
+        kafka_broker=KAFKA_BROKER,
+        mds_port=MDS_PORT,
+        principal=PRINCIPAL,
+        role=ROLE,
+        mds_username=MDS_USERNAME,
+        mds_password=MDS_PASSWORD
+    )
+    import pprint
+
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(res)
